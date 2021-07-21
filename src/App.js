ee28b6776c5f4ada37dc65c466f992c0769aba1e
Elvira Snezhkova <eli4kina@gmail.com>
2021-07-21 14:41:37 +0700

    Удалила все ненужные файлы

--- src/App.js
+++ src/App.js
@@ -1,37 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './number';
+import React from 'react';
+let element = (<div>Hello World!</div>);
 
-const day=14;
-const dayOfWeek='Tuesday';
-const currentYear=2021;
-const yearOfBirth=1989;
-let flag=true;
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div style={{textAlign: 'left'}}>
-        <p>Произвндение чисел = {count*length}</p>
-        </div>
-
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
