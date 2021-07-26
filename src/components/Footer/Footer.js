8d5195b896cabe76ef7b00d06951f88556f132ba
Elvira Snezhkova <eli4kina@gmail.com>
2021-07-26 22:45:59 +0700

    использовала готовые библиотеки компонентов Material UI для поля ввода и кнопок

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import  styles from  './Footer.module.css';
+import styles from  './Footer.module.css';
+import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { purple } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#8e49e8',
+    },
+    secondary: {
+      main: '#f04eb2',
+    },
+  },
+});
 
 const Footer = ( {count} ) => (
   <footer className={styles.footer}>
   <div> {count} items left</div>
-  <button className={styles.btn}>All</button>
-  <button className={styles.btn}>Active</button>
-  <button className={styles.btn}>Completed</button>
-  <button className={styles.btn}>Clear completed</button>
+  <ThemeProvider theme={theme}>
+    <Button variant="outlined" color="primary" size="small">All</Button>
+    <Button variant="outlined" color="primary" size="small">Active</Button>
+    <Button variant="outlined" color="primary" size="small">Completed</Button>
+  </ThemeProvider>
+  
+  <Button color="secondary" size="small">Clear completed</Button>
 </footer>	
 );
 
