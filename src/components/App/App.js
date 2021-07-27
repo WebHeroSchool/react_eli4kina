b92cffcf427357147e26e32cb7d63728282c4841
Elvira Snezhkova <eli4kina@gmail.com>
2021-07-27 21:36:00 +0700

    Превращение функции App в компонент класс

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,29 +4,32 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-  const todoItems = [
-    {
-      value: 'Сделать зарядку',
-      isDone: true
-    },
-    {
-      value: 'Забрать пальто из химчистки',
-      isDone: false
-    },
-    {
-      value: 'Отвезти кота в вет клинику',
-      isDone: false
-    }
-  ];
-  
-  return (
-  <div className={styles.wrapper}>
-    <h2 className={styles.title}>Todos</h2>
-    <InputItem />
-    <ItemList todoItems = {todoItems} />
-    <Footer count={3} />
-  </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    const todoItems = [
+      {
+        value: 'Сделать зарядку',
+        isDone: true
+      },
+      {
+        value: 'Забрать пальто из химчистки',
+        isDone: false
+      },
+      {
+        value: 'Отвезти кота в вет клинику',
+        isDone: false
+      }
+    ];
+    
+    return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>Todos</h2>
+      <InputItem />
+      <ItemList todoItems = {todoItems} />
+      <Footer count={3} />
+    </div>
+    );
+  }
+};
+
 export default App;
